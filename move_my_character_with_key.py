524b492b5bd06b1c7924c3df1e531b74b91774e3
황상준 <hsj45935@gmail.com>
2025-09-25 20:17:44 +0900

    캔버스 열기 및 이미지 로드

---
+++ move_my_character_with_key.py
@@ -0,0 +1,10 @@
+from pico2d import *
+
+width, height = 1280, 1024
+
+open_canvas(width,height)
+
+tuk_ground = load_image('TUK_GROUND.png')
+character = load_image('character.png')
+
+close_canvas()
