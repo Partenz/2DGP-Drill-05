80fa0f6f6ca89a0261c4e932fc6aab460ea86275
황상준 <hsj45935@gmail.com>
2025-09-25 20:26:48 +0900

    핸들 이벤트 뼈대 작성

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -8,6 +8,26 @@ tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('character.png')
 
 
+def handle_event():
+    global running
+
+    events = get_events()
+
+    for event in events:
+        if (event.type == SDL_QUIT)
+            running = False
+        elif(event.type == SDL_KEYDOWN):
+            if(event.key == SDLK_ESCAPE):
+                running = False
+            elif(event.key == SDLK_LEFT):
+                pass
+            elif(event.key == SDLK_RIGHT):
+                pass
+            elif(event.key == SDLK_UP):
+                pass
+            elif(event.key == SDLK_DOWN):
+                pass
+
 running = True
 
 frame = 0
@@ -16,7 +36,7 @@ while running:
     clear_canvas()
     tuk_ground.draw(width // 2, height // 2)
 
-
     update_canvas()
+    handle_event()
 
 close_canvas()
